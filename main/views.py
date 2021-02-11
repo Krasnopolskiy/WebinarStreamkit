e9599ae8cfdc782ecbf640212b76c6cbbbb60cb3
Krasnopolskiy Ivan <i1.krasnopolskiy@gmail.com>
2021-02-11 13:41:06 +0000

    Small updates

--- main/views.py
+++ main/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 from django.http import HttpRequest
 from django.http.response import HttpResponse
 from django.views import View
+
 from . import forms
+
 
 class IndexView(View):
     context = {'pagename': 'Index'}
 
     def get(self, request: HttpRequest) -> HttpResponse:
         return render(request, 'pages/index.html', self.context)
+
 
 class SignupView(View):
     context = {'pagename': 'Signup'}
