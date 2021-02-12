e686a4549ac638d4dfee70390433c3c5d89c3b53
Krasnopolskiy Ivan <i1.krasnopolskiy@gmail.com>
2021-02-12 21:50:16 +0300

    Add logout views

--- webinar_streamkit/urls.py
+++ webinar_streamkit/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from main import views
@@ -7,5 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', views.LoginView.as_view(), name='login'),
     path('signup/', views.SignupView.as_view(), name='signup'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('', views.IndexView.as_view(), name='index'),
 ]
