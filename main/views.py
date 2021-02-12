d6e1cea29741057087334f84a29969ab6beb5291
Borovickaya Polina <forgamesinclub@gmail.com>
2021-02-12 11:11:39 +0300

    Backend registration

--- main/views.py
+++ main/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest
 from django.http.response import HttpResponse
 from django.views import View
-
+from django.contrib.auth import login, authenticate, get_user_model
 from . import forms
+from django.urls import reverse
 
 
 class IndexView(View):
@@ -18,4 +19,18 @@ class SignupView(View):
 
     def get(self, request: HttpRequest) -> HttpResponse:
         self.context['form'] = forms.SignupForm()
+        return render(request, 'registration/signup.html', self.context)
+
+    def post(self, request: HttpRequest) -> HttpResponse:
+        get_user_model()
+        form = forms.SignupForm(request.POST)
+        self.context['form'] = form
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse('index'))
         return render(request, 'registration/signup.html', self.context)
