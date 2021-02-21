5c8a22dce6eb983b80cbd46bebafcdab2729a467
Borovickaya Polina <forgamesinclub@gmail.com>
2021-02-21 21:36:21 +0300

    First version of the webinar list

--- webinar_streamkit/urls.py
+++ webinar_streamkit/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('change-password/', auth_views.PasswordChangeView.as_view(), name='change_password'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('schedule/', views.ScheduleView.as_view(), name='schedule'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.IndexView.as_view(), name='index'),
 ]
