a51d8a5fa82c31dbdb09d800102bfabb73fa4780
Artem Glazyrin <artemglazyrin@mail.ru>
2021-02-21 22:46:09 +0300

    Modify SignUpView

--- main/views.py
+++ main/views.py
@@ -5,9 +5,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpRequest
 from django.http.response import HttpResponse
 from django.views import View
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.contrib import messages
 from django.contrib.auth.views import LoginView
+from django.views.generic import CreateView
 
 from . import forms
 
@@ -27,34 +28,39 @@ class CustomLoginView(SuccessMessageMixin, LoginView):
     success_message = 'Пользователь успешно авторизован'
 
 
-class SignupView(View):
+class SignupView(SuccessMessageMixin, CreateView):
     context = {'pagename': 'Signup'}
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('login')
+    form_class = forms.SignupForm
+    success_message = "Пользователь успешно зарегистрирован"
+#    messages.add_message(self.request, messages.ERROR, 'We could not process your request at this time.')
 
-    def get(self, request: HttpRequest) -> HttpResponse:
-        self.context['form'] = forms.SignupForm()
-        return render(request, 'registration/signup.html', self.context)
-
-    def post(self, request: HttpRequest) -> HttpResponse:
-        get_user_model()
-        form = forms.SignupForm(request.POST)
-        self.context['form'] = form
-        if form.is_valid():
-            user = form.save()
-            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
-            messages.add_message(
-                request,
-                messages.SUCCESS,
-                'Пользователь успешно зарегистрирован',
-                extra_tags='alert-success'
-            )
-            return redirect(reverse('index'))
-        messages.add_message(
-            request,
-            messages.ERROR,
-            'Ошибка регистрации',
-            extra_tags='alert-danger'
-        )
-        return render(request, 'registration/signup.html', self.context)
+    # def get(self, request: HttpRequest) -> HttpResponse:
+    #     self.context['form'] = forms.SignupForm()
+    #     return render(request, 'registration/signup.html', self.context)
+    #
+    # def post(self, request: HttpRequest) -> HttpResponse:
+    #     get_user_model()
+    #     form = forms.SignupForm(request.POST)
+    #     self.context['form'] = form
+    #     if form.is_valid():
+    #         user = form.save()
+    #         login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+    #         messages.add_message(
+    #             request,
+    #             messages.SUCCESS,
+    #             'Пользователь успешно зарегистрирован',
+    #             extra_tags='alert-success'
+    #         )
+    #         return redirect(reverse('index'))
+    #     messages.add_message(
+    #         request,
+    #         messages.ERROR,
+    #         'Ошибка регистрации',
+    #         extra_tags='alert-danger'
+    #     )
+    #     return render(request, 'registration/signup.html', self.context)
 
 
 class ProfileView(View):
