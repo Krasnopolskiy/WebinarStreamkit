3ace1c4efb3f6a49f69f5b061ad93c50dca79d59
Krasnopolskiy Ivan <i1.krasnopolskiy@gmail.com>
2021-02-22 10:22:20 +0300

    Fix models

--- main/models.py
+++ main/models.py
@@ -1,2 +1,6 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+class AdditionalUserInfo(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    avatar = models.CharField(max_length=255)
