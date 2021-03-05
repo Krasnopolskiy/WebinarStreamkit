605ea0c806d5799ad20fa6dae40f1aa0c5335658
Borovickaya Polina <forgamesinclub@gmail.com>
2021-03-05 23:46:37 +0300

    Developments on the webinar page

--- main/views.py
+++ main/views.py
@@ -54,8 +54,15 @@ class ProfileView(View):
 
 class EventView(View):
     context = {'pagename': 'Event'}
+    sessionid='6c4221eb82804f97a6b6e27c7a005ee5'
 
     def get(self, request: HttpRequest) -> HttpResponse:
+        information = request.get('https://events.webinar.ru/api/eventsession/8454775').json()
+        self.context['name'] = information['name']
+        self.context['data_webinar'] = information['startsAt']
+        self.context['webinar_host'] = information['organization']['name']
+        self.context['web_status'] = information['status']
+
         return render(request, 'pages/event.html', self.context)
 
 class ScheduleView(View):
