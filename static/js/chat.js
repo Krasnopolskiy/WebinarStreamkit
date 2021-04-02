e5587fcb625090a31f141ee0dff72ae89cb318a8
Artem Glazyrin <artemglazyrin@mail.ru>
2021-04-02 09:24:36 +0300

    Fix awaiting scrolling

--- static/js/chat.js
+++ static/js/chat.js
@@ -26,15 +26,20 @@ let update_event_handlers = () => {
         console.log(`Declining <Message ${message_id}> ...`)
     })
 }
-let last_scroll_pos = 0;
+let last_scroll_pos_moder = 0;
+let last_scroll_pos_await = 0;
 ws.onmessage = event => {
     let data = JSON.parse(event['data'])
     if (data['event'] == 'update chat') {
         update_chat(data['template']);
-        document.querySelector('#moderated').scrollTop = last_scroll_pos;
+        document.querySelector('#moderated').scrollTop = last_scroll_pos_moder;
+        document.querySelector('#awaiting').scrollTop = last_scroll_pos_await;
     }
     $('#moderated').on("scroll", function(event) {
-        last_scroll_pos = $('#moderated').scrollTop();
+        last_scroll_pos_moder = $('#moderated').scrollTop();
+    });
+    $('#awaiting').on("scroll", function(event) {
+        last_scroll_pos_await = $('#awaiting').scrollTop();
     });
 }
 
