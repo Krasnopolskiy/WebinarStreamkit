8c4ff9c7f357b5d434de081cedd2a41980a36d20
Artem Glazyrin <artemglazyrin@mail.ru>
2021-04-14 23:02:55 +0300

    Add warning for popup windows

--- static/js/event.js
+++ static/js/event.js
@@ -2,6 +2,15 @@ let url = new URL(window.location.href)
 
 $('#widget').click(event => {
     let height = 600, width = 400
-    window.open(`${url.href}/moderated`, 'Moderated chat', `height=${height}, width=${width}, top=0, left=0`)
-    window.open(`${url.href}/awaiting`, 'Awaiting chat', `height=${height}, width=${width}, top=0, left=${width + 20}`)
+    w1 = window.open(`${url.href}/moderated`, 'Moderated chat', `height=${height}, width=${width}, top=0, left=0`)
+    w2 = window.open(`${url.href}/awaiting`, 'Awaiting chat', `height=${height}, width=${width}, top=0, left=${width + 20}`)
+    if (!w1 || !w2)
+    {
+        if (!w1 && w2)
+            w2.close()
+        else if (!w2 && w1)
+            w1.close()
+        alert('Внимание! У вас отключены всплывающие окна. \nЧтобы продолжить работу включите их')
+    }
+
 })
