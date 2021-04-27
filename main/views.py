2b9bc8b04d64766278ff79e1e4362bcb18336a6a
Krasnopolskiy Ivan <i1.krasnopolskiy@gmail.com>
2021-04-27 12:25:05 +0300

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import LoginView, PasswordChangeView
+from django.contrib.auth.views import LoginView
 from django.http import HttpRequest
 from django.http.response import HttpResponse, HttpResponsePermanentRedirect
 from django.shortcuts import redirect, render
@@ -12,6 +12,28 @@ from django_registration.backends.one_step.views import RegistrationView
 from main.forms import (ExtendedRegistrationForm, UserInformationForm,
                         WebinarCredentialsForm)
 from main.webinar import BaseRouter
+
+
+class ExtendedLoginView(LoginView):
+    def post(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        response = super(LoginView, self).post(request, *args, **kwargs)
+        if request.user.is_authenticated:
+            messages.add_message(request, messages.SUCCESS, 'Авторизация прошла успешно')
+        else:
+            messages.add_message(request, messages.ERROR, 'Неверное имя пользователя или пароль. Повторите попытку')
+        return response
+
+
+class ExtendedRegistrationView(RegistrationView):
+    def post(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        response = super(RegistrationView, self).post(request, *args, **kwargs)
+        form = ExtendedRegistrationForm(request.POST)
+        if request.user.is_authenticated:
+            messages.add_message(request, messages.SUCCESS, 'Регистрация прошла успешно')
+        for scope in form.errors.values():
+            for error in list(scope):
+                messages.add_message(request, messages.ERROR, error)
+        return response
 
 
 class IndexView(View):
@@ -120,25 +142,3 @@ class ControlView(LoginRequiredMixin, View):
     def get(self, request: HttpRequest, event_id: int) -> HttpResponse:
         self.context['event'] = request.user.webinar_session.get_event(event_id)
         return render(request, 'pages/control.html', self.context)
-
-
-class ExtendedLoginView(LoginView):
-    def post(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
-        response = super(LoginView, self).post(request, *args, **kwargs)
-        if request.user.is_authenticated:
-            messages.add_message(request, messages.SUCCESS, 'Авторизация прошла успешно')
-        else:
-            messages.add_message(request, messages.ERROR, 'Неверное имя пользователя или пароль. Повторите попытку')
-        return response
-
-
-class ExtendedRegistrationView(RegistrationView):
-    def post(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
-        response = super(RegistrationView, self).post(request, *args, **kwargs)
-        form = ExtendedRegistrationForm(request.POST)
-        if request.user.is_authenticated:
-            messages.add_message(request, messages.SUCCESS, 'Регистрация прошла успешно')
-        for scope in form.errors.values():
-            for error in list(scope):
-                messages.add_message(request, messages.ERROR, error)
-        return response
