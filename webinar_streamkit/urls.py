0600eecb4cb569ac7033cf7ae86fbe89a8f978b8
Borovickaya Polina <forgamesinclub@gmail.com>
2021-05-12 22:35:46 +0300

    Edit code for debug_toolbar

--- webinar_streamkit/urls.py
+++ webinar_streamkit/urls.py
@@ -1,8 +1,9 @@
+import debug_toolbar
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.views import LogoutView, PasswordChangeView
-from django.urls import path, reverse_lazy
+from django.urls import path, reverse_lazy, include
 
 from main.consumers import (AwaitingMessagesConsumer, ChatConsumer,
                             ControlConsumer)
@@ -41,6 +42,9 @@ urlpatterns = [
 
 
 if settings.DEBUG:
+    urlpatterns = [
+        path('__debag__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
     urlpatterns += static(
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
