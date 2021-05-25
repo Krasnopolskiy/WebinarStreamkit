ed72ccefff2a00fdc62cef1ef77e787e8c03f5dc
Krasnopolskiy Ivan <i1.krasnopolskiy@gmail.com>
2021-05-25 12:48:18 +0300

    Update runner

--- ci/readthedocs/runner.sh
+++ ci/readthedocs/runner.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-git remote add github https://Krasnopolskiy:$GITHUB_ACCESS_TOKEN@github.com/Krasnopolskiy/WebinarStreamkit.git
+git remote add github 'https://Krasnopolskiy:$GITHUB_ACCESS_TOKEN@github.com/Krasnopolskiy/WebinarStreamkit.git'
+echo 'https://Krasnopolskiy:$GITHUB_ACCESS_TOKEN@github.com/Krasnopolskiy/WebinarStreamkit.git'
 git push github HEAD:master
 exit 0
