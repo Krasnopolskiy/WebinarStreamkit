f7cbf5f18605372773f2ec44be6d46892fc78ebd
Krasnopolskiy Ivan <i1.krasnopolskiy@gmail.com>
2021-05-27 16:10:38 +0300

    Fix js

--- static/js/messages.js
+++ static/js/messages.js
@@ -1,6 +1,6 @@
 let last_scroll_pos = 0
 let url = new URL(window.location.href)
-let ws = new WebSocket(`ws://${url.host}${url.pathname}`)
+let ws = new WebSocket(`wss://${url.host}${url.pathname}`)
 
 let update_messages = template => {
     if (template !== $('#message-box').html()) {
